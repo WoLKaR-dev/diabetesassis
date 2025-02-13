8cf8fee0e04c9b82e98b6dba06d2eef839b59654
WoLKaR <bswacko@gmail.com>
2025-02-13 22:40:46 +0100

    modified:   fastRationsCalculator.html 	modified:   rationsCalculatorHandler.js 	modified:   stylesHandler.css

--- rationsCalculatorHandler.js
+++ rationsCalculatorHandler.js
@@ -1,47 +1,89 @@
-function showCalculator(tipeOfCalculator){
+var isFat = false;
+
+function checkedCheckbox() {
+    isFat = !isFat;
+    let simpleFatOption = document.getElementById('simpleFatOption');
+    let advancedFatOption = document.getElementById('advancedFatOption');
+
+    if (isFat == true) {
+        simpleFatOption.style.display = 'flex'
+        advancedFatOption.style.display = 'flex'
+    } else {
+        simpleFatOption.style.display = 'none'
+        advancedFatOption.style.display = 'none'
+    }
+
+}
+
+
+function showCalculator(tipeOfCalculator) {
     let simple = document.getElementById('simpleCalculator');
     let advanced = document.getElementById('advancedCalculator');
     simple.style.display = 'none';
     advanced.style.display = 'none';
 
-    if(tipeOfCalculator == 'simple'){
+    if (tipeOfCalculator == 'simple') {
         simple.style.display = 'flex'
-    }else{
+    } else {
         advanced.style.display = 'flex'
     }
 }
 
 
 
-function returnClassicRations(){
+function returnClassicRations() {
     let foodAmount = parseFloat(document.getElementById('simpleFoodAmount').value);
     let each100 = parseFloat(document.getElementById('simpleEach100').value);
+    let fatEach100 = parseFloat(document.getElementById('simpleFatEach100').value);
+    let result = 0;
+    let fatRations = ((foodAmount/100)*fatEach100)/10;
 
-    if (isNaN(foodAmount)|| isNaN(each100)){
-        return 0; 
+
+
+    if (isNaN(foodAmount) || isNaN(each100)) {
+        return 0;
     }
 
-    return (foodAmount/100) * each100 / 10;
+    if (isFat){
+        result = ((foodAmount / 100) * each100 / 10)+fatRations;
+    }else{
+        result = (foodAmount / 100) * each100 / 10;
+    }
+
+
+    return result;
 }
 
-function returnAdvancedRations(){
+function returnAdvancedRations() {
     let foodAmount = parseFloat(document.getElementById('advancedFoodAmount').value);
     let each100 = parseFloat(document.getElementById('advancedEach100').value);
+    let fatEach100 = parseFloat(document.getElementById('advancedFatEach100').value);
+    let result = 0;
+    let fatRations = ((foodAmount/100)*fatEach100)/10;
 
-    if (isNaN(foodAmount)|| isNaN(each100)){
-        return 0; 
+
+
+    if (isNaN(foodAmount) || isNaN(each100)) {
+        return 0;
     }
 
-    return (foodAmount/100) * each100 / 10;
+    if (isFat){
+        result = ((foodAmount / 100) * each100 / 10)+fatRations;
+    }else{
+        result = (foodAmount / 100) * each100 / 10;
+    }
+
+
+    return result;
 }
 
 
-function simpleCalc(){
+function simpleCalc() {
     let amount = returnClassicRations();
-    document.getElementById('finalResult').innerText = 'Raciones calculadas: '+ amount;
+    document.getElementById('finalResult').innerText = 'Raciones calculadas: ' + amount;
 }
 
-function agregarFila(){
+function agregarFila() {
     let table = document.getElementById("foodTable").querySelector('tbody');
     let row = document.createElement('tr');
     let tdName = document.createElement("td");
